e62c2f8f0954d977bd5b372bab07c18487a52265
guoqiang <guoqiang.yang@biz-united.com.cn>
2017-12-07 10:55:04 +0800

    Initial commit

---
+++ src/main/java/com/biz/controller/TestHello.java
@@ -0,0 +1,13 @@
+package com.biz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestHello {
+    @RequestMapping("/textHello")
+    public String test() {
+        System.out.println("#########");
+        return "hello";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 杨国强
+  Date: 2017/12/6
+  Time: 19:05
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>hello</title>
+</head>
+<body>
+    <h2>Hello World</h2>
+</body>
+</html>
